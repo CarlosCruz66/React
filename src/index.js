a0a747e5da56f4d04bdfe5b47ce570f705f43ee5
CarlosCruz <ccruz@sinube.mx>
2024-09-13 17:59:29 -0600

    IntroJavaScript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 // import { heroes} from './data/heroes';
-import {heroes} from './data/heroes'
-console.log(heroes);
+// import {heroes} from './data/heroes';
+
+
+import { heroes } from "./data/heroes";
+
+// const getHeroeByI = (id)=>{
+//     return heroes.find((heroe)=>{
+//         if(heroe,id===id){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     });
+// }
+
+const getHeroeById = (id)=> heroes.find((heroe)=>heroe.id===id);
+
+
+
+console.log(getHeroeById(2));
+
+const getHeroesByOwner=(owner)=>heroes.find((heroe)=>heroe.id===id);
+
+console.log(getHeroesByOwner('DC'));
